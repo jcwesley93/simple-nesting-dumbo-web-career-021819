6d98b8cca4fa98d0cee68da47d1e6d2cc05380bb
Jaela Wesley <jcwesley93@gmail.com>
2019-02-05 14:32:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]["C"].join
+	programmer_hash[:dennis_ritchie][:languages].join
+	
 end
 
 def adding_matz
